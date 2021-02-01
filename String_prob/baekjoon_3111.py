deae81fbbd9513c5becc59929c43f6032b45e79c
Jin_Hwan <astrowh@naver.com>
2021-02-01 22:09:02 +0900

    extend를 활용해서 시간 절약

--- String_prob/baekjoon_3111.py
+++ String_prob/baekjoon_3111.py
@@ -3,12 +3,14 @@
 
 from collections import deque
 
-# p = '#' + input()
-# o = list(reversed(input()))
-p = '#' + 'abc'
-# o = list(reversed('abc'*1000))
-o = deque('abc'*100000)
+# p = deque(input())
+# o = deque(input())
 
+p = 'abc'
+o = deque('asdfgsdfgbc'*100000+'d')
+# 이문제는 KMP를 쓰지 않더라도 30만 *25라 천만을 넘지않음, NM =10000000, 그러므로 잘 해주면되는데 문자열 복사가 넘 오래 걸림
+# 링크드 리스트를 쓰면 빨리 될 거 같기는 한데, 구현이 너무 빡셈,
+# 연결 부위가 찾는 문자열이 아님을 증명하면 될듯?
 # return은 p를 o에서 모두 제거
 
 def pi(s):
@@ -45,32 +47,95 @@ flag = 1
 
 # 건너뛰기 위해 while문을 활용
 # 정방향만 생각
-while o:
-    if flag ==1:
-        c=0
-    else:
-        c=-1
-    for k in range(1,len(p)):
-        if o[c] == p[k*flag]:
-            c+= 1*flag
-        else:
-            break
-    if c == len(p)-1 or  c == -len(p):
-        for _ in range(len(p)-1):
-            if flag ==1:
-                o.popleft()
+
+# while o:
+#     if flag ==1:
+#         c=0
+#     else:
+#         c=-1
+#     for k in range(1,len(p)):
+#         if o[c] == p[k*flag]:
+#             c+= 1*flag
+#         else:
+#             break
+#     if c == len(p)-1 or  c == -len(p):
+#         for _ in range(len(p)-1):
+#             if flag ==1:
+#                 o.popleft()
+#             else:
+#                 o.pop()
+#         o = stack_l + o + stack_r
+#         stack_l = deque()
+#         stack_r = deque()
+#         flag*=-1
+#     else:
+#         if flag == 1:
+#             stack_l.append(o.popleft())
+#         else:
+#             stack_r.appendleft(o.pop())
+# print(stack_l + stack_r)
+
+
+def left(o,p):
+    while o: 
+    # 이게 너무 오래 걸리는거 같당
+        c= 0
+
+        for k in range(len(p)):
+            if len(o)> c and o[c] == p[k]:
+                c+=1
             else:
-                o.pop()
-        o = stack_l + o + stack_r
-        stack_l = deque()
-        stack_r = deque()
-        flag*=-1
-    else:
-        if flag == 1:
+                break
+        if c == len(p):
+            for _ in range(len(p)):
+                o.popleft()
+            return 1
+        else:
             stack_l.append(o.popleft())
+    return 0
+
+
+def right(o,p):
+    while o:
+        c= -1
+        for k in range(len(p)-1,-1,-1):
+            if len(o) >= abs(c) and o[c] == p[k]:
+                # 아 길이가 안될 수도 있네
+                c-=1
+            else:
+                break
+        if c == -len(p)-1:
+            for _ in range(len(p)):
+                o.pop()
+            return 1
         else:
             stack_r.appendleft(o.pop())
-print(stack_l + stack_r)
+    return 0
 
 
+stack_l = deque()
+stack_r = deque()
+l_cursor = 0
+r_curosr = -1
+while True:
+    # stack_l에 값이 있으면 합쳤을때 key가 생기는지 검사
+    if stack_l:
 
+    if left(o,p):
+        o.extendleft(stack_l) 
+        # 이부분이 진짜 오래걸림
+        stack_l = deque()
+        if stack_r:
+        if right(o,p):
+            o.extend(stack_r) 
+            # + 연산은 해악이다. 절대 하지말것!
+            # 여기가 문제다
+            stack_r = deque()
+            continue
+        else:
+            break
+    else:
+        break
+o.extendleft(stack_l) 
+o.extend(stack_r) 
+print(''.join(list(o)))
