afc07c47cfc3eca0374063466825356326dc5f3a
Jin_Hwan <astrowh@naver.com>
2021-02-02 15:41:43 +0900

    선언할때 값을 추가하는 식으로 바꿈

--- String_prob/baekjoon_1620.py
+++ String_prob/baekjoon_1620.py
@@ -4,16 +4,18 @@ sys.stdin = open('String_prob/baekjoon_1620.txt')
 N,M = map(int, input().split())
 
 pokedex = dict()
-
+# 여기에서 먼저 선언하면 되는데
+pokenum = dict()
 for n in range(N):
-    pokedex[input()] = n+1
+    temp = input()
+    pokedex[temp] = n+1
+    pokenum[n+1] = temp
 # dict의 items는 tuple을 return하므로 unpacking 해주면된다.
 # 특이하게 문자열 숫자를 key값으로 넣어주었는데, 알아서 int로 바뀐다.
-pokenum = { val:key for key, val in pokedex.items()}
 
 for _ in range(M):
     temp = input()
-    if pokedex.get(temp):
-        print(pokedex[temp])
-    else:
+    if temp.isdigit():
         print(pokenum[int(temp)])
+    else:
+        print(pokedex[temp])
