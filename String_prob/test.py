d50a555fa2a680dfdda8ab7394592f329bbcc7e3
Jin_Hwan <astrowh@naver.com>
2021-02-04 18:00:03 +0900

    test 생성

--- String_prob/test.py
+++ String_prob/test.py
@@ -1,4 +1,9 @@
-a= 'fasas'*300000
-
-temp = list(reversed(a))
-print(a[0])
+text = '100000\n1 2\n1 3\n'
+for i in range(2, 99999):
+    text+=f'{i} {i+2}\n'
+text += '6\n'
+for i in range(5):
+    text+=f'{i+1} {i+3}\n'
+text += f'5 6'
+with open('test.txt','w') as f:
+    f.write(text)
