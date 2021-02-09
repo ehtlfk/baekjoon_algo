690317cb5306b553d58c64c11e7af3ab5ef199ef
Jin_Hwan <astrowh@naver.com>
2021-02-09 17:53:45 +0900

    문제를 제대로 읽자 mn 범위를 잘못줌

--- DP_prob/baekjoon_2651.py
+++ DP_prob/baekjoon_2651.py
@@ -7,36 +7,36 @@ K = int(input())
 s_num = int(input())
 
 D = list(map(int, input().split(' ')))
-d = 0
 stops = list(map(int, input().split(' ')))
 
 S = [0]*(s_num+1)
-ans = set()
+ans = [[] for _ in range(s_num+1)]
+d = D[0]
 
-d = D[0]+D[1]
-if D[0]+D[1] >K:
-    S[1]=stops[0]
-    ans.add(1)
-    d = D[1]
-
-    
-for i in range(2,s_num+1):
+for i in range(1,s_num+1):
     d += D[i]
     if d>K:
-        s1 = S[i-1]+stops[i-1]
-        s2 = S[i-2]+stops[i-2]
-        # 정비소를 들리고 d를 안뺌
-        # 아 같은 경우...
-        S[i]= min(s1,s2)
-        if s1 <= s2:
-            ans.add(i)
-            d = D[i]
-        else:
-            ans.add(i-1)
-            d=D[i] + D[i-1]
-            
+        mn = K+1
+        c = 0
+        d = D[i]
+        for j in range(i-1,-1,-1):
+            if d >K:
+                break
+            temp = S[j]+stops[j]
+            if mn > temp:
+                c = j
+                mn = temp
+            d+=D[j]
+        S[i] = mn
+        ans[i].extend(ans[c])
+        ans[i].append(c+1)
+        d = sum(D[c+1:i+1]) 
     else:
         S[i] = S[i-1]
+        ans[i].extend(ans[i-1])
 print(S[-1])
-print(len(ans))
-print(' '.join(map(str,ans)))
+
+print(len(ans[-1]))
+if len(ans[-1]):
+    print(' '.join(map(str,ans[-1])))
+
