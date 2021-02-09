c62641b00c604257f1b4118f4e007e69e7a2c9f7
Jin_Hwan <astrowh@naver.com>
2021-02-09 14:36:00 +0900

    틀림 반례를 찾아서

--- DP_prob/baekjoon_2651.py
+++ DP_prob/baekjoon_2651.py
@@ -7,41 +7,35 @@ K = int(input())
 s_num = int(input())
 
 D = list(map(int, input().split(' ')))
-d_stack = [0]*(s_num+1)
-d_stack[0] = D[0]
-for i in range(1,s_num+1):
-    d_stack[i]=D[i]+d_stack[i-1]
+d = 0
 stops = list(map(int, input().split(' ')))
 
 S = [0]*(s_num+1)
 ans = set()
-if d_stack[1] >K:
+
+d = D[0]+D[1]
+if D[0]+D[1] >K:
     S[1]=stops[0]
     ans.add(1)
+    d = D[1]
 
+    
 for i in range(2,s_num+1):
-    l = d_stack[i]//K
-    mn = 10000000
-    c = 0
-    for j in range(l):
-        if D[i-l]+D[i+l-1] <= K:
-            temp = S[i-l]+stops[i-l]
-            if mn > temp:
-                mn = temp
-                c = i-l+1
-            # s2 = S[i-2]+stops[i-2]
-            # S[i]= min(s1,s2)
-            # if s1> s2:
-            #     ans.add(i-1)
-            # else:
-            #     ans.add(i)
+    d += D[i]
+    if d>K:
+        s1 = S[i-1]+stops[i-1]
+        s2 = S[i-2]+stops[i-2]
+        # 정비소를 들리고 d를 안뺌
+        S[i]= min(s1,s2)
+        if s1 < s2:
+            ans.add(i)
+            d = D[i]
         else:
-            S[i] = S[i-l]+stops[i-l]
-            ans.add(i-l+1)
-    if l:
-        ans.add(c)
-        S[i] = mn
-print(S)
+            ans.add(i-1)
+            d=D[i] + D[i-1]
+            
+    else:
+        S[i] = S[i-1]
+print(S[-1])
 print(len(ans))
 print(' '.join(map(str,ans)))
-print(stops, d_stack, D)
