9c41c91eb0b1ec712c1b76c9357c9b070ed26f56
Jin_Hwan <astrowh@naver.com>
2021-03-06 13:27:08 +0900

    해결

--- programmers/순위.py
+++ programmers/순위.py
@@ -24,11 +24,102 @@ d = [0,4,4,3,2,1,1,0,0]
 # answer = l
 # print(answer)
 
-check = [1]*(len(d))
-for i in range(1,len(d)):
-    for j in range(i+1, len(d)):
-        if d[i] == d[j]:
-            check[d[i]] = 0
-answer = sum(check)-1
-print(answer)
-print(check)
+# check = [1]*(len(d))
+# for i in range(1,len(d)):
+#     for j in range(i+1, len(d)):
+#         if d[i] == d[j]:
+#             check[d[i]] = 0
+# answer = sum(check)-1
+# print(answer)
+# print(check)
+
+
+
+def dfs(r,g,d,p):
+    for e in g[r]:
+        if d[e] == 0:
+            d[e] = d[r]+1
+            p[e] = r
+            dfs(e,g,d,p)
+        elif d[e] != d[r]-1:
+            d[e] = d[r]+1
+            
+def solution(n, results):
+    
+    g = [[] for _ in range(n+1)]
+    r = [0]*(n+1)
+    roots =[]
+    for res in results:
+        s,f = res
+        g[f].append(s)
+        r[s] = 1
+    for i in range(1,n+1):
+        if r[i] == 0:
+            roots.append(i)
+    cnt = []
+    d = [0]*(n+1)
+    p = [0]*(n+1)
+    
+    for root in roots:
+        dfs(root,g,d,p)
+        
+    check = [1]*(max(d)+1)
+    for i in range(1,len(d)):
+        for j in range(i+1, len(d)):
+            if d[i] == d[j]:
+                check[d[i]] = 0
+    answer = sum(check)
+    
+    
+    leaf = []
+    for i in range(1,n+1):
+        if not g[i]:
+            leaf.append(i)
+    
+    if len(leaf) > 1 and check[max(d)] == 1:
+        answer-=1
+    
+    print(d)  
+        
+    return answer
+
+
+def bfs(r,g,n):
+    queue = [r]
+    v= [0]*(n+1)
+    while queue:
+        temp = queue.pop(0)
+        for e in g[temp]:
+            if v[e] == 0:
+                v[e] = 1
+                queue.append(e)
+    return sum(v)
+        
+            
+def solution(n, results):
+    answer = 0
+    win = [[] for _ in range(n+1)]
+    lose = [[] for _ in range(n+1)]
+    win_cnt = [0]*(n+1)
+    lose_cnt = [0]*(n+1)
+    
+    for res in results:
+        s,f = res
+        win[f].append(s)
+        lose[s].append(f)
+    for i in range(1,n+1):
+        win_cnt[i] = bfs(i,win,n)
+    for i in range(1,n+1):
+        lose_cnt[i] = bfs(i,lose,n)
+        
+    for x,y in zip(win_cnt,lose_cnt):
+        if x+y == n-1:
+            answer+=1
+        
+    return answer
+
+
+    # 이 문제의 핵심은 한 정점이 이긴 정점과 진 정점의 개수 합이 N-1일 경우 순위가 결정이 되는 것이다. 이것을 위해 단방향 그래프 두 개를 이용하여 한 쪽은 이기는 정점, 반대쪽은 지는 정점의 개수를 조사하도록 코드를 작성. 
+    # 모든 정점에 대해 자신이 이기는 정점과 지는 정점을 bfs나 dfs를 이용하여 개수를 셈. 단방향 그래프이기 때문에 다른 방향으로는 가지 않는다.
+
+    # 모든 정점에 대해 탐색을 하므로 속도가 느리다. 부모와 조상 관계로 이것을 줄일 수도 있을 것 같다.
