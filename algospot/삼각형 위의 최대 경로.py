5310acf5e2389f677ae19ce4c534dc50f4d0b655
Jin_Hwan <astrowh@naver.com>
2021-03-10 11:20:41 +0900

    예제

---
+++ algospot/삼각형 위의 최대 경로.py
@@ -0,0 +1,17 @@
+import sys, os
+
+BASE_DIR = os.path.splitext(os.path.realpath(__file__))[0] +  '.txt'
+sys.stdin = open(BASE_DIR)
+input = sys.stdin.readline
+# extend는 None을 리턴해서 0으로 했는데, 먼가 아쉽
+
+
+
+for _ in range(int(input())):
+    N = int(input())
+    # tree = [[0]*N for _ in range(N)]
+    tree = [list(map(int,input().split()))+ [0]*(N-i-1) for i in range(N) ] 
+    for i in range(1,N):
+        for j in range(i+1):
+            tree[i][j] += max(tree[i-1][j],tree[i-1][j-1]) 
+    print(max(tree[-1]))
