47a67de101e6cb2d01e691fd9775aee3c82abbb8
Jin_Hwan <astrowh@naver.com>
2021-03-10 10:27:30 +0900

    N^3

--- algospot/와일드 카드.py
+++ algospot/와일드 카드.py
@@ -3,44 +3,44 @@ import sys, os
 BASE_DIR = os.path.splitext(os.path.realpath(__file__))[0] +  '.txt'
 sys.stdin = open(BASE_DIR)
 
-def check(word1, word2):
-    cnt = 0
-    i = 0
-    while i <len(word2):
-        cnt = 0
-        for j in range(len(word1)):
-            if word1[j] == word2[i] or word1[j] =='?':
-                cnt+=1
-                i+=1
-                continue
-            else:
-                break
-        if cnt == len(word1):
-            return i
+def check(pos1, pos2):
+    
+    i = pos1
+    j = pos2
+    while j < len(word) and i < len(wild) and (wild[i] == word[j] or wild[i] == '?'):
         i+=1
-    return -1
+        j+=1
+    if i == len(wild):
+        if j == len(word):
+            return 1
+        else:
+            return 0
+
+    if wild[i]=='*':
+        for k in range(len(word)):
+            # 음, 넘어가는 거 s+k <len(word)를 넣으면, *로 끝날 때 0을 리턴한다
+            if check(i+1 ,j+k):
+                return 1
+    return 0
 for _ in range(int(input())):
     wild = input()
     N = int(input())
     words = []
+    wild_list = []
 
-    s_wild = wild.split('*')
+    # pos = 0
+    # for i in range(len(wild)):
+    #     if wild[i] == '*':
+    #         wild_list.append(wild[pos:i+1])
+    #         pos = i+1
+
+    # if not wild_list:
+    #     wild_list = [wild]         
 
     for _ in range(N):
         word = input()
-        i = 0
-        for ch in s_wild:
-            if not ch:
-                continue
-            
-            temp = check(ch, word[i:])
-            i = temp+1
-            if temp != -1:
-                continue
-            else:
-                break
-        else:
-            
+        if check(0,0):
             words.append(word)
+        
     for w in sorted(words):
         print(w)
