a0e4ad1da9ddd26c38d2cec7ed38d234d27513ed
Jin_Hwan <astrowh@naver.com>
2021-03-10 15:41:57 +0900

    자신 보다 작은 수의 LIS의 최대값 +1 => N^2

--- algospot/최대 증가 부분 수열.py
+++ algospot/최대 증가 부분 수열.py
@@ -3,12 +3,20 @@ import sys, os
 BASE_DIR = os.path.splitext(os.path.realpath(__file__))[0] +  '.txt'
 sys.stdin = open(BASE_DIR)
 
+
+# i를 끝으로 하는 LIS 값이 LIS[i] 
 for _ in range(int(input())):
     N = int(input())
     seq = list(map(int,input().split()))
     LIS = [1]*N
     for i in range(1,N):
-        if seq[i-1]< seq[i]:
-            LIS[i] = LIS[i-1]+1
+        mx = 0
+        for j in range(i-1,-1,-1):
+            if seq[j] < seq[i]:
+                if mx < LIS[j]:
+                    mx = LIS[j]
+        LIS[i] = mx+1
+
+ 
     print(max(LIS))
-    
+    print(LIS)
