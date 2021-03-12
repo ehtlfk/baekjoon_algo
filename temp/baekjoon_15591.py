cac85d4881e92b626e4fcbdeac7bfdff65b72e27
Jin_Hwan <astrowh@naver.com>
2021-03-12 17:52:26 +0900

    싸이클이 있나보네

--- temp/baekjoon_15591.py
+++ temp/baekjoon_15591.py
@@ -7,24 +7,29 @@ input = sys.stdin.readline
 # 어떤 동영상에서 다른 동영상으로 가는 경로가 반드시 하나 존재,full인가 complete인가 conneted graph
 # usado는 모든 usado 중 최솟값
 # 단위가 매우 큼 최적화 필요
-def dfs(s,node,mn):
-    v[node] = 1
-    for i in range(s+1,N+1):
-        if v[i] == 0 and graph[node][i] != float('inf'):
-            graph[s][i] = min(mn,graph[node][i])
-            graph[i][s] = min(mn,graph[node][i])
-            dfs(s,i,graph[node][i])
+def dfs(s,f):
+    if graph[s][f] != float('inf'):
+        return graph[s][f]
+    for next in adj_list[s]:
+        ret = min(graph[s][next],dfs(next,f))
+    graph[s][f] = ret
+    graph[f][s] = ret
+    return ret
+    
 N, Q = map(int,input().split())
 graph = [[float('inf')]*5001 for _ in range(5001)]
-
+adj_list =[[] for _ in range(N+1)]
 for _ in range(N-1):
     p,q,r = map(int,input().split())
     graph[p][q] = min(graph[p][q], r)
     graph[q][p] = min(graph[q][p], r)
+    adj_list[p].append(q)
+    adj_list[q].append(p)
 
 for i in range(1,N+1):
-    v= [0]*(N+1)
-    dfs(i,i,float('inf'))
+    for j in range(i+1,N+1):
+        if graph[i][j] == float('inf'):
+            dfs(i,j)
     
 for _ in range(Q):
     K,V = map(int,input().split())
