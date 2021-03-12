2c8b68c9ef21f694547be98f9ac03fd2b60fca2f
Jin_Hwan <astrowh@naver.com>
2021-03-12 22:09:00 +0900

    min(int, none) type error

--- temp/baekjoon_15591.py
+++ temp/baekjoon_15591.py
@@ -8,19 +8,25 @@ input = sys.stdin.readline
 # usado는 모든 usado 중 최솟값
 # 단위가 매우 큼 최적화 필요
 def dfs(s,f):
+    v[s] = 1
     if graph[s][f] != float('inf'):
         return graph[s][f]
     for next in adj_list[s]:
-        ret = min(graph[s][next],dfs(next,f))
-    graph[s][f] = ret
-    graph[f][s] = ret
-    return ret
+        if v[next] == 0:
+            ret = min(graph[s][next],dfs(next,f))
+            graph[s][f] = ret
+            graph[f][s] = ret
+    # for next in adj_list[s]:
+    #     ret = min(graph[s][next],dfs(next,f))
+    # graph[s][f] = ret
+    # graph[f][s] = ret
+
 
 def bfs(s,f):
     v[s] = 1
-    queue = adj_list[s]
-    while queue:
-        temp = queue.pop(0)
+    queue = [(s,f)]
+    while queue: # s==f일경우
+        s,f = queue.pop(0)
         for next in adj_list[temp]:
             if graph[s][next] == float('inf') and v[next] == 0:
                 v[next] = 1
@@ -33,19 +39,21 @@ def bfs(s,f):
 N, Q = map(int,input().split())
 graph = [[float('inf')]*5001 for _ in range(5001)]
 adj_list =[[] for _ in range(N+1)]
+roots = [0]*(N+1)
 for _ in range(N-1):
     p,q,r = map(int,input().split())
     graph[p][q] = min(graph[p][q], r)
     graph[q][p] = min(graph[q][p], r)
     adj_list[p].append(q)
+    roots[q] = 1
     adj_list[q].append(p)
 
 for i in range(1,N+1):
     for j in range(i+1,N+1):
         if graph[i][j] == float('inf'):
             v = [0]*(N+1)
-            bfs(i,j)
-    
+            dfs(i,j)
+# dfs(1,0)
 for _ in range(Q):
     K,V = map(int,input().split())
     cnt=0
