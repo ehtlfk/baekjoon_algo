0e7c8fa03fdbd5a46a49195a0470f7740fd251d5
Jin_Hwan <astrowh@naver.com>
2021-03-13 16:11:52 +0900

    push용

--- temp/baekjoon_15591.py
+++ temp/baekjoon_15591.py
@@ -9,24 +9,29 @@ input = sys.stdin.readline
 # 단위가 매우 큼 최적화 필요
 def dfs(s,f):
     v[s] = 1
-    if graph[s][f] != float('inf'):
-        return graph[s][f]
-    for next in adj_list[s]:
-        if v[next] == 0:
-            ret = min(graph[s][next],dfs(next,f))
-            graph[s][f] = ret
-            graph[f][s] = ret
+    # if graph[s][f] != float('inf'):
+    #     return graph[s][f]
+    for n in adj_list[s]:
+        if v[n] == 0:
+            ret = min(graph[prev][s], dfs(n, f))
+            graph[prev][n] = ret
+            graph[n][prev] = ret
+    node.append(s)
+    # return graph[prev][s]
     # for next in adj_list[s]:
     #     ret = min(graph[s][next],dfs(next,f))
     # graph[s][f] = ret
     # graph[f][s] = ret
 
 
-def bfs(s,f):
+def bfs(s):
     v[s] = 1
-    queue = [(s,f)]
+    for i in adj_list[s]:
+        v[i] = 1
+    queue = adj_list[s]
+
     while queue: # s==f일경우
-        s,f = queue.pop(0)
+        temp = queue.pop(0)
         for next in adj_list[temp]:
             if graph[s][next] == float('inf') and v[next] == 0:
                 v[next] = 1
@@ -39,27 +44,29 @@ def bfs(s,f):
 N, Q = map(int,input().split())
 graph = [[float('inf')]*5001 for _ in range(5001)]
 adj_list =[[] for _ in range(N+1)]
-roots = [0]*(N+1)
+
 for _ in range(N-1):
     p,q,r = map(int,input().split())
     graph[p][q] = min(graph[p][q], r)
     graph[q][p] = min(graph[q][p], r)
     adj_list[p].append(q)
-    roots[q] = 1
     adj_list[q].append(p)
 
-for i in range(1,N+1):
-    for j in range(i+1,N+1):
-        if graph[i][j] == float('inf'):
-            v = [0]*(N+1)
-            dfs(i,j)
-# dfs(1,0)
+# for i in range(1,N+1):
+    # for next in adj_list[i]:
+v = [0]*(N+1)
+node = []
+dfs(1)
+print(list(reversed(node)))
 for _ in range(Q):
     K,V = map(int,input().split())
     cnt=0
     for i in range(1,N+1):
-        if graph[V][i] >=K:# 자기 자신도 포함했구나..
+        if V!=i and graph[V][i]>=K:
             cnt+=1
+    # for i in range(1,N+1):
+    #     if graph[V][i] >=K:# 자기 자신도 포함했구나..
+    #         cnt+=1
 
     print(cnt-1)
 
