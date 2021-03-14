da6859839efcd9127ca793bf9eeb8f3a3d2c21e5
Jin_Hwan <astrowh@naver.com>
2021-03-14 20:49:34 +0900

    simple is best

--- temp/baekjoon_11000.py
+++ temp/baekjoon_11000.py
@@ -13,18 +13,14 @@ for _ in range(N):
     course.append((s,t))
 s_course = sorted(course, key=lambda x:(x[0],x[1]))
 
-cnt = 1
 queue = [s_course[0][1]]
+cnt = 1
+
 for i in range(1,N):
-    if s_course[i][0] == s_course[i][1]:
-        continue
-    if s_course[i-1][0] == s_course[i][0] and s_course[i-1][0]!=s_course[i-1][1]:
-        cnt+=1
-    elif queue[0] > s_course[i][0]:
+    if queue[0] > s_course[i][0]:
         cnt+=1
     elif queue[0] <= s_course[i][0]:
-        heapq.heappop(queue)
-        
+        heapq.heappop(queue) 
     heapq.heappush(queue,s_course[i][1])
     # print(queue)
 print(cnt)
