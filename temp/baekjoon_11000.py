5038152e1437090430ce67c4c3df48f8b47cd38c
Jin_Hwan <astrowh@naver.com>
2021-03-14 20:49:23 +0900

    S==T는 heap에 추가해서는 안됨

--- temp/baekjoon_11000.py
+++ temp/baekjoon_11000.py
@@ -3,7 +3,7 @@ BASE_DIR = os.path.splitext(os.path.realpath(__file__))[0] +  '.txt'
 sys.stdin = open(BASE_DIR)
 input = sys.stdin.readline
 
-import heapq
+import heapq # min heap을 기준으로 함
 
 
 N = int(input())
@@ -16,15 +16,17 @@ s_course = sorted(course, key=lambda x:(x[0],x[1]))
 cnt = 1
 queue = [s_course[0][1]]
 for i in range(1,N):
+    if s_course[i][0] == s_course[i][1]:
+        continue
     if s_course[i-1][0] == s_course[i][0] and s_course[i-1][0]!=s_course[i-1][1]:
         cnt+=1
     elif queue[0] > s_course[i][0]:
         cnt+=1
-    else:
+    elif queue[0] <= s_course[i][0]:
         heapq.heappop(queue)
-
+        
     heapq.heappush(queue,s_course[i][1])
-    
+    # print(queue)
 print(cnt)
 
     
