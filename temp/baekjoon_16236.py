8fb31f461fb3b7bd5016095c2acd2f1f31592458
Jin_Hwan <astrowh@naver.com>
2021-03-18 16:40:28 +0900

    while True 하고 t를 1씩 증가시키면, 먹이가 막혀있는 경우를 측정할 수 없음

--- temp/baekjoon_16236.py
+++ temp/baekjoon_16236.py
@@ -14,8 +14,7 @@ from collections import deque
 
 # 우선 위치에서 제일 가까운 먹을 수 있는 먹이 탐색, 발견하면 거기로 이동
 # 다시 그 위치에서 제일 가까운 거 탐색
-N = int(input())
-mat = [list(map(int,input().split())) for _ in range(N)]
+
 
 def bfs(i,j):
     dx = [0,1,0,-1] # 우하좌상, 이걸 조절해도 안됨, 어느 점의 왼쪽은 어느 점의 위쪽일 수도 있음
@@ -23,23 +22,23 @@ def bfs(i,j):
     t = 0
     queue = deque([(i,j)])
     size = 2
-    cnt = [i for i in range(100)]
     v = [[0]*N for _ in range(N)]
-    v[i][j] = 1
+    cnt = [k for k in range(100)]
+   
     while True:
-        l = len(queue) 
+        l= len(queue)
         if sum(feeds[:size]) == 0:
             return t
         # 갈 수 있는 경로 찾기  
-        mnx = 30
-        mny = 30
+        mnx = 3000
+        mny = 3000
         for _ in range(l):
             x,y = queue.popleft()
             for k in range(4):
                 nx = dx[k]+x
                 ny = dy[k]+y
                 if 0<=nx<N and 0<=ny<N and mat[nx][ny] <= size and v[nx][ny] == 0:
-                    v[nx][ny] = 1
+                    v[nx][ny] = v[x][y]+1
                     queue.append((nx,ny))
                     if 0 < mat[nx][ny] < size:
                         if mnx > nx:
@@ -50,20 +49,22 @@ def bfs(i,j):
                                 mnx = nx
                                 mny = ny 
         
-        if mnx != 30:
+        if mnx < 3000:
+            t+=v[mnx][mny]
             queue = deque([(mnx,mny)])
-            l = len(queue)
             feeds[mat[mnx][mny]] -=1
             mat[mnx][mny] = 0
             cnt[size]-=1
             if cnt[size] == 0:
                 size+=1
             v = [[0]*N for _ in range(N)]
-            v[mnx][mny] = 1
-            
-        t+=1
+
+        if len(queue) == 0: # 움직일 수 없는 경우
+            return t
         
-    
+        
+N = int(input())
+mat = [list(map(int,input().split())) for _ in range(N)]
 bs = tuple()
 feeds = [0]*7 
 for i in range(N):
