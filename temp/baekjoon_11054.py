12ef9eafd8ae24ca2651401eaa7975838b973471
Jin_Hwan <astrowh@naver.com>
2021-03-19 15:56:19 +0900

    시간초과

--- temp/baekjoon_11054.py
+++ temp/baekjoon_11054.py
@@ -9,39 +9,52 @@ input = sys.stdin.readline
 # 최대값이 여러개
 # 이어져 있지 않은 부분 수열
 # 최대값을 기준으로, 최대값이 Sk오른쪽이면 Sk는 최대값이어야 최대 길이,  왼쪽일 경우 역시, Sk 가 최대값이어야 최대 길이
+
+# 반례 9 1 2 3 4 5 6
+
 # 그러면 최대값을 구하고, 최대값을 제외한 상태에서 바이토닉 최장 길이를 구함 => 재귀인가?
 # 각 order에 해당하는 index값을 저장해야하나?
 
 # 현재 위치에서 자신보다 큰 값의 개수를 구함, 메모이제이션을 해야하나?
 
-def bio(k,n,seq): # n: 최대값 idx
-    if k == n-1:
-        return 1
+def bio(k,n): # n: 최대값 idx
+    if cache[k][n]!=-1:
+        return cache[k][n]
     mx = 0
-    for i in range(k+1,n):
-        if seq[i] == seq[n]:
-            continue
-        elif seq[k] < seq[i]:
-            mx = max(mx,bio(i,n,seq))
-    return mx+1
+
+    for i in range(k+1,n+1):
+        if seq[k] < seq[i]:
+            mx = max(mx,bio(i,n)+1)
+
+    if cache[k][n] == -1:
+        cache[k][n] = mx
+    return mx
+
+def bio2(k,n):
+    if cache[k][n]!=-1:
+        return cache[k][n]
+    mx = 0
+    for i in range(k-1,n-1,-1):
+        if seq[k] < seq[i]:
+            mx = max(mx,bio2(i,n)+1)
+    if cache[k][n] == -1:
+        cache[k][n] = mx
+    return mx
 
 N = int(input())
-
 seq = list(map(int,input().split())) # mx
-rseq = list(reversed(seq)) # N-1-mx
 
-
-# print(bio(len(order)-1))
-mx = 0
-mx_list= []
-for i in range(N):
-    if seq[i] > mx:
-        mx = seq[i]
-        mx_list = []
-        mx_list.append(i)
-    elif seq[i] == mx:
-        mx_list.append(i)
 ans = 0
-for x in mx_list:
-    ans = max(ans,(bio(0,x,seq)+bio(0,N-1-x,rseq)+1))
-print(ans)
+cache = [[-1]*N for _ in range(N)]
+for x in range(N):
+    
+    m1 = 0
+    m2 = 0  
+    for i in range(x):
+        m1 = max(m1,bio(i,x))
+    for k in range(N-1,x,-1):
+        m2 = max(m2,bio2(k,x))
+    
+    ans = max(ans,m1+m2)
+
+print(ans+1)
