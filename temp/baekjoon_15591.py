fc0f1d74fc5dba9556d38aeee67247767b42576a
Jin_Hwan <astrowh@naver.com>
2021-06-12 17:20:09 +0900

    lca로 도전

--- temp/baekjoon_15591.py
+++ temp/baekjoon_15591.py
@@ -7,6 +7,8 @@ input = sys.stdin.readline
 # 어떤 동영상에서 다른 동영상으로 가는 경로가 반드시 **하나** 존재,full인가 complete인가 conneted graph
 # usado는 모든 usado 중 최솟값
 # 단위가 매우 큼 최적화 필요
+# 한 노드에서 다르 노드로 가는 경로가 단 하나! 
+# -> 트리, 각 노드의 조상을 구하고 최소값을 구해놓은 뒤, lca를 구하면 리프 노드에서 리프 노드까지의 유사도를 알 수 있다.
 def dfs(s,f):
     v[s] = 1
     # if graph[s][f] != float('inf'):
@@ -24,7 +26,7 @@ def dfs(s,f):
     # graph[f][s] = ret
 
 
-def bfs(s):
+def bfs(s, f):
     v[s] = 1
     for i in adj_list[s]:
         v[i] = 1
@@ -32,6 +34,8 @@ def bfs(s):
 
     while queue: # s==f일경우
         temp = queue.pop(0)
+        if temp == f:
+            break
         for next in adj_list[temp]:
             if graph[s][next] == float('inf') and v[next] == 0:
                 v[next] = 1
@@ -54,10 +58,13 @@ for _ in range(N-1):
 
 # for i in range(1,N+1):
     # for next in adj_list[i]:
-v = [0]*(N+1)
-node = []
-dfs(1)
-print(list(reversed(node)))
+
+for i in range(1,N+1):
+    v = [0]*(N+1)
+    bfs(i)
+for i in range(1,5):
+    print(graph[i][1:5])
+
 for _ in range(Q):
     K,V = map(int,input().split())
     cnt=0
@@ -68,7 +75,7 @@ for _ in range(Q):
     #     if graph[V][i] >=K:# 자기 자신도 포함했구나..
     #         cnt+=1
 
-    print(cnt-1)
+    print(cnt)
 
 
     # 아 node 5000개...
