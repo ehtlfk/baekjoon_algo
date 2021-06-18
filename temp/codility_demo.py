0d6735a5372a3e01d78d78e943f15a47bddc4d46
Jin_Hwan <astrowh@naver.com>
2021-06-18 15:08:50 +0900

    메모리 절약

--- temp/codility_demo.py
+++ temp/codility_demo.py
@@ -1,12 +1,13 @@
 def solution(A):
     # write your code in Python 3.6
-    num = [0]*100001
+    # num = [0]*100001
+    mx = max(A)
 
-    for i in range(len(A)):
-        if A[i]>0:
-            num[A[i]] = 1
-    for j in range(1,100001):
-        if not num[j]:
+    # for i in range(len(A)):
+    #     if A[i]>0:
+    #         num[A[i]] = 1
+    for j in range(1,mx+1):
+        if j not in A:
             return j
 
 
@@ -15,3 +16,4 @@ def solution(A):
 # 정수의 범위는 -1,000,000 ~ 1,000,000 이므로 백만까지 배열을 다 채울 수 없기 때문에 1000001 출력은 생각안해도 된다.
 
 # 위 코드에서 개선점은 굳이 메모리를 1000001개 쓸 필요가 없다. max값을 구해서 max값 만큼 돌리면 된다.
+# 그럴 경우 시간에서 손해가 발생
