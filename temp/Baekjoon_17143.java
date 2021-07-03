c6ef54df2363bc2a8bb40ebb230a61bb5580da5d
Jin_Hwan <astrowh@naver.com>
2021-07-03 10:59:41 +0900

    StringTokenizer와 Arrays를 이용한 array 출력

--- temp/Baekjoon_17143.java
+++ temp/Baekjoon_17143.java
@@ -1,9 +1,10 @@
 package temp;
-
+// bufferedReader가 scanner보다 빠르지만, type을 바꿔줘야하는 번거로움 존재
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
-
+import java.util.StringTokenizer;
+import java.util.Arrays;
 
 class Baekjoon_17143 {
     public static void main(String args[]) throws Exception {
@@ -14,13 +15,18 @@ class Baekjoon_17143 {
         int R = Integer.parseInt(input[0]);
         int C = Integer.parseInt(input[1]);
         int M = Integer.parseInt(input[2]);
+
+        int[][] arr = new int[M][5];
         for (int i=0; i<M; ++i) {
-            String[] sangu = bReader.readLine().split(" ");
+            StringTokenizer st = new StringTokenizer(bReader.readLine()," ");
+            for (int j=0; j<5; ++j){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }   
         } 
         long answer = 0;
         
 
-        System.out.println(R);
+        System.out.println(Arrays.toString(arr[0]));
       
     }
 }
